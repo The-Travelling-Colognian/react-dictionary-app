da739a32c1c2745842c1b501ef9a7cd44073ee6f
Vanessa Schade <82608228+travellingcolognian@users.noreply.github.com>
2021-09-29 16:08:03 +0200

    Created the Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://modest-lamarr-257cde.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vanessa Schade
+          </a>
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/The-Travelling-Colognian"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://infallible-visvesvaraya-096ea3.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
